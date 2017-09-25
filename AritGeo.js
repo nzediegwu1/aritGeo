4ae19992ef3618d75785a5d8169213db89bcee14
nzediegwu1 <nzediegwu1@outlook.com>
2017-09-25 18:46:17 +0100

    Ft(AritGeo Function): Define method for AritGeo functionality

---
+++ AritGeo.js
@@ -0,0 +1,28 @@
+﻿const AritGeo = (array) => {
+    if (array.length === 0) {
+        return 0;
+    }
+    const arithArray = [];
+    const geomArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (i > 1) {
+            const aritListElement = array[i] - array[i - 1];
+            arithArray.push(aritListElement);
+            const geomListElement = array[i] / array[i - 1];
+            geomArray.push(geomListElement);
+        }
+    }
+    const arithSet = new Set(arithArray);
+    if (arithSet.size === 1) {
+        return 'Arithmetic';
+    }
+    else {
+        const geomSet = new Set(geomArray);
+        if (geomSet.size === 1) {
+            return 'Geometric';
+        }
+        else {
+            return -1;
+        }
+    }
+};
